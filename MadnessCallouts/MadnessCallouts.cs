491079e0b3f9ae6baa9df55c32dba3e3f0308d69
DocDev <doc.dev.fr@gmail.com>
2017-08-13 19:32:57 +0200

    Undo deleting of csproj file

--- MadnessCallouts/MadnessCallouts.cs
+++ MadnessCallouts/MadnessCallouts.cs
@@ -1,36 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Windows.Forms;
 using LSPD_First_Response.Mod.API;
-using Rage;
 
 namespace MadnessCallouts
 {
 	public class Main : Plugin
 	{
 		public override void Initialize()
-        {
-            Functions.OnOnDutyStateChanged += OnOnDutyStateChangedHandler;
-            //Game.LogTrivial(System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString());
-        }
-        public override void Finally()
-        {
+		{
+			Functions.OnOnDutyStateChanged += OnOnDutyStateChangedHandler;
+			//Game.LogTrivial(System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString());
+		}
+		public override void Finally()
+		{
             
-        }
+		}
 
-        private static void OnOnDutyStateChangedHandler(bool OnDuty)
-        {
-            if (OnDuty)
-            {
-                RegisterCallouts(); 
-            }
-        }
+		private static void OnOnDutyStateChangedHandler(bool OnDuty)
+		{
+			if (OnDuty) {
+				RegisterCallouts(); 
+			}
+		}
 
-        private static void RegisterCallouts()
-        {
-            Functions.RegisterCallout(typeof(Callouts.DangerousIndividual));
+		private static void RegisterCallouts()
+		{
+			Functions.RegisterCallout(typeof(Callouts.DangerousIndividual));
 		}
 	}
 }
